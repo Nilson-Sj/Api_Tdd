6e75a1f7db79992862e9cf26a422a03c67f33602
Nilson-Sj <nilsonjr180795@gmail.com>
2022-11-09 10:32:34 -0300

    Add Todo-Tree

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function helloWorld(){
+    
+    // TODO resolver isso aqui
     console.log('Hello World!');
 }
 
 const saudacao = () => {
+    // FIXME melhorar esse código
     var data = new Date()
     return data.getHours() <= 12? "Bom dia!": data.getDate() <= 18? "Boa tarde!": "Boa noite!";
 }
