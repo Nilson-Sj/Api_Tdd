739126727efa499bcc3e1d97548ff8bb0aeca6dd
Nilson-Sj <nilsonjr180795@gmail.com>
2022-11-17 11:52:50 -0300

    Add method post and consign with middlewares

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@
 /* eslint-disable indent */
 // eslint-disable-next-line no-undef
 const app = require('express')();
+const consign = require('consign');
+
+consign({ cwd: 'src', verbose: false })
+  .include('./config/middlewares.js')
+  .into(app);
 
 app.get('/', (req, res) => {
   res.status(200).send();
@@ -15,6 +20,10 @@ app.get('/users', (req, res) => {
     { name: 'John Doe', mail: 'john@mail.com' },
   ];
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
