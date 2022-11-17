73a5024da55250d42ab8d53ee4a1d10517c9da79
Nilson-Sj <nilsonjr180795@gmail.com>
2022-11-17 11:24:02 -0300

    add rules in comments

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
+/* eslint-disable semi */
 /* eslint-disable indent */
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 
-const app = require("./app")
+const app = require("./app");
 
 app.listen(3001, () => {
   console.log("Aplicação está no AR!")
-})
+});
