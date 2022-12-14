8a4c45b25de81eabba8c028d6f9d771a244a332c
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-14 11:54:19 -0300

    Add knex-logger in app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /* eslint-disable quotes */
 /* eslint-disable no-unused-vars */
 /* eslint-disable semi */
@@ -8,10 +9,12 @@ const app = require('express')();
 const consign = require('consign');
 const knex = require('knex');
 const knexfile = require('../knexfile');
+//const knexLogger = require('knex-logger');
 
 // TODO criar chaveamento dinâmico
 app.db = knex(knexfile.test);
 
+//app.use(knexLogger(app.db));
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
@@ -22,5 +25,11 @@ consign({ cwd: 'src', verbose: false })
 app.get('/', (req, res) => {
   res.status(200).send();
 });
+
+//app.db.on('query', (query) => {
+//  console.log({ sql: query.sql, bindings: query.bindings ? query.bindings.join(','): ''})
+// })
+//  .on('query-response', response => console.log(response))
+//  .on('error', error => console.log(error));
 
 module.exports = app;
