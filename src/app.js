f0993708a9ec4011aff236b6f8856abdc3ee7b08
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-14 11:35:20 -0300

    Add knex and knexfile in app.js

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@
 // eslint-disable-next-line no-undef
 const app = require('express')();
 const consign = require('consign');
+const knex = require('knex');
+const knexfile = require('../knexfile');
+
+// TODO criar chaveamento dinâmico
+app.db = knex(knexfile.test);
+
 
 consign({ cwd: 'src', verbose: false })
   .include('./config/middlewares.js')
