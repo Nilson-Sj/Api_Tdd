776a0e66bdc159df50b50f27209ad48244351365
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-16 09:28:16 -0300

    Created existing user validation

--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -8,6 +8,8 @@ const request = require('supertest');
 
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@mail.com`
+
 
 test('Deve listar todos os usuários', () => {
   return request(app).get('/users')
@@ -18,7 +20,6 @@ test('Deve listar todos os usuários', () => {
 });
 
 test('Deve inserir usuário com sucesso!', () => {
-  const mail = `${Date.now()}@mail.com`
   return request(app).post('/users')
     .send({ name: 'Walter Mitty', mail, passwd: '123456' })
     .then((res) => {
@@ -53,5 +54,14 @@ test('Não deve inserir usuário sem senha', (done) => {
       done();
     })
     .catch(err => done.fail(err));
+});
+
+test('Não deve inserir usuário com email existente', () => {
+  return request(app).post('/users')
+    .send({ name: 'Walter Mitty', mail, passwd: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(400);
+      expect(res.body.error).toBe('Já existe um usuário com esse email')
+    });
 });
 
