c7b69233b313af71a198956b276134283bae1799
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-16 12:53:30 -0300

    Created return an account by id

--- src/config/routes.js
+++ src/config/routes.js
@@ -10,4 +10,7 @@ module.exports = (app) => {
   app.route('/accounts')
     .get(app.routes.accounts.getAll)
     .post(app.routes.accounts.create);
+
+  app.route('/accounts/:id')
+    .get(app.routes.accounts.get);
 };
