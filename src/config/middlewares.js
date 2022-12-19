90798745f0d36ea60be85b8b765cd2b70654ee92
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-19 12:21:16 -0300

    Moved knexlogger from app.js to middlewares.js

--- src/config/middlewares.js
+++ src/config/middlewares.js
@@ -3,7 +3,10 @@
 /* eslint-disable quotes */
 /* eslint-disable no-undef */
 const bodyParser = require('body-parser');
+//const knexLogger = require('knex-logger');
+
 
 module.exports = (app) => {
   app.use(bodyParser.json());
+  //app.use(knexLogger(app.db));
 };
