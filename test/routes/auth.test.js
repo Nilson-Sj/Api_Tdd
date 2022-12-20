3d2fe0734e4ec685998f94eb99fea78ca8c3b246
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-20 11:49:29 -0300

    Created user authentication test by signup

--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -1,12 +1,23 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
+test('Deve criar usuário via signup', () => {
+  return request(app).post('/auth/signup')
+    .send({ name: 'Walter', mail: `${Date.now()}@mail.com`, passwd: '123456' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Walter');
+      expect(res.body).toHaveProperty('mail');
+      expect(res.body).not.toHaveProperty('passwd');
+    });
+});
+
 test('Deve receber token ao logar', () => {
   const mail = `${Date.now()}@mail.com`;
   return app.services.user.save(
     { name: 'Walter', mail, passwd: '123456' })
     .then(() => request(app).post('/auth/signin')
-    .send({ mail, passwd: '123456' }))
+      .send({ mail, passwd: '123456' }))
     .then((res) => {
       expect(res.status).toBe(200);
       expect(res.body).toHaveProperty('token');
@@ -18,7 +29,7 @@ test('Não deve autenticar usuário com senha errada', () => {
   return app.services.user.save(
     { name: 'Walter', mail, passwd: '123456' })
     .then(() => request(app).post('/auth/signin')
-    .send({ mail, passwd: '654321' }))
+      .send({ mail, passwd: '654321' }))
     .then((res) => {
       expect(res.status).toBe(400);
       expect(res.body.error).toBe('Usuário ou senha inválido');
@@ -30,6 +41,13 @@ test('Não deve autenticar usuário com senha errada', () => {
     .send({ mail: 'naoExiste@mail.com', passwd: '654321' })
     .then((res) => {
       expect(res.status).toBe(400);
-      expect(res.body.error).toBe('Usuário ou senha inválido');      
+      expect(res.body.error).toBe('Usuário ou senha inválido');
+    });
+});
+
+test('Não deve acessar uma rota protegida sem token', () => {
+  return request(app).get('/users')
+    .then((res) => {
+      expect(res.status).toBe(401);
     });
 });
