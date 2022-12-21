888e5bb491841e19289d0908743a93b5ab0847e5
Nilson-Sj <nilsonjr180795@gmail.com>
2022-12-21 08:49:49 -0300

    Created test to not duplicate user account

--- src/services/account.js
+++ src/services/account.js
@@ -5,12 +5,6 @@
 const ValidationError = require('../errors/ValidationError');
 
 module.exports = (app) => {
-  const save = async (account) => {
-    if(!account.name) throw new ValidationError('Nome é um atributo obrigatório');
-    
-    return app.db('accounts').insert(account, '*');
-  };
-
   const findAll = (userId) => {
     return app.db('accounts').where({ user_id: userId });
   };
@@ -19,19 +13,28 @@ module.exports = (app) => {
     return app.db('accounts').where(filter).first();
   };
 
+  const save = async (account) => {
+    if (!account.name) throw new ValidationError('Nome é um atributo obrigatório');
+
+    const accDb = await find({ name: account.name, user_id: account.user_id });
+    if (accDb) throw new ValidationError('Já existe uma conta com esse nome');
+
+    return app.db('accounts').insert(account, '*');
+  };
+
   const update = (id, account) => {
     return app.db('accounts')
-    .where({ id })
-    .update(account, '*');
+      .where({ id })
+      .update(account, '*');
   };
 
   const remove = (id) => {
     return app.db('accounts')
-    .where({ id })
-    .del();
+      .where({ id })
+      .del();
   };
 
-  return { 
+  return {
     save, findAll, find, update, remove,
   };
 };
